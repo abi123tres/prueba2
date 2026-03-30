0a10b0844cfca32076639ae63afe41946628977c
Abigail <abigail.cabanillas@utec.edu.pe>
2026-03-29 20:23:56 -0500

    Intentomil

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ print("Hola cara d bola")
 #Cambios en rama 2
 <<<<<<< HEAD
 #QUIEN UN CONFLICT
+#Yo solo quiero q me salga q hay un conflicto
 =======
 #quiero un problema para resolver
 >>>>>>> ramaprueba
